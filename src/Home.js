db81859f1bb0addc29af56c19ca4ec08071f8c13
yoel <yoel@macbook-pro.home>
2020-08-28 10:52:46 -0700

    [home] add Home component

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Annalissa Dentistry</h1>
+        <p>Where the dentists go for their dental needs.</p>
+      </div>
+    )
+  }
+}
+
+export default Home;
