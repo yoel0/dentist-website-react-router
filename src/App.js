3610208a06da4ca59e9abd3e81d286477022dbef
yoel <yoel@macbook-pro.home>
2020-08-28 11:10:41 -0700

    [add] routes to components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import './App.css';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+
+
 import Home from './Home';
 import Procedures from './Procedures';
 import Contact from './Contact';
 
 function App() {
   return (
-    <div className="App">
-    <Home />
-    <Procedures />
-    <Contact />
-    </div>
+    <Router>
+      <div className="App">
+        <Route exact path="/" component={Home} />
+        <Route path="/procedures" component={Procedures} />
+        <Route path="/contact" component={Contact} />
+      </div>
+    </Router>
   );
 }
 
